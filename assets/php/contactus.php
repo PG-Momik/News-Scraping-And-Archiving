7d4d9c65e86368d147c5bbc26568ff135d2706d5
Igris <igris@pop-os.localdomain>
2021-08-08 18:33:22 +0545

    fixed contactus

--- assets/php/contactus.php
+++ assets/php/contactus.php
@@ -5,7 +5,7 @@
 	<title>Contact us</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="finalcss.css">	
+    <link rel="stylesheet" type="text/css" href="../css/style.css">	
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light nav-container">
@@ -24,7 +24,7 @@
                     </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link action" href="index.php">Home</a>
+                    <a class="nav-link action" href="./././index.php">Home</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="aboutus.php">About us</a>
@@ -128,6 +128,6 @@
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="darkmode.js"></script>
+<script src="../js/darkmode.js"></script>
 
 </html>
