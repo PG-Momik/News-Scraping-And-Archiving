2137ba8505a7b3ac9236cb523e73c7d265abb7a4
Igris <igris@pop-os.localdomain>
2021-08-08 18:35:13 +0545

    fixed drop_table

--- assets/php/drop_table.php
+++ assets/php/drop_table.php
@@ -5,6 +5,6 @@ if(isset($_POST['drop-table-btn'])){
     $name=$_POST['drop-table-hidden-val'];
     $sql = "DROP TABLE ".$name;
     $result = mysqli_query($conn, $sql);
-    header('Loaction:http://localhost/loginpage/action_table.php');
+    header('Loaction:http://localhost/WebScraping/php/action_table.php');
 }
 ?>
