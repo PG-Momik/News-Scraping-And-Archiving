8e8fe4b499f83d9caca93b549e30ef2c4a652991
Igris <igris@pop-os.localdomain>
2021-08-08 17:58:59 +0545

    updates

--- assets/php/action_scraper.php
+++ assets/php/action_scraper.php
@@ -13,11 +13,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Scraper Parameters</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">	
-    <link rel="stylesheet" type="text/css" href="finalcss.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
 <?php 
-    include 'navigation.php';
+    include '../php/partials/navigation.php';
 ?>
 <div class="container-fluid">
 <br>
@@ -88,12 +88,12 @@
     
     </div>
 </div>    
-<?php include 'footer.php';?>
+<?php include '../php/partials/footer.php';?>
 </body>
 </html>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="darkmode.js"></script>
+    <script src="../js/darkmode.js"></script>
 
 <?php
     }
--- assets/php/action_table.php
+++ assets/php/action_table.php
@@ -29,10 +29,10 @@ if(mysqli_num_rows($result) > 0){
     <title>Table Control</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="finalcss.css">	
+    <link rel="stylesheet" type="text/css" href="../css/style.css">	
 </head>
 <body>
-    <?php include "navigation.php"?>
+    <?php include "../php/partials/navigation.php"?>
     <div class="container-fluid">
         <h1 align="center">Manage Tables</h1>
         <div id="table-control-container">
@@ -101,6 +101,6 @@ if(mysqli_num_rows($result) > 0){
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="darkmode.js"></script>
+<script src="../js/darkmode.js"></script>
 </html>
 <?php }?>
