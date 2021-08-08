e11295ed659918465f457e65e8232b8a0b183c47
Igris <igris@pop-os.localdomain>
2021-08-08 18:50:40 +0545

    fixed update_user

--- assets/php/update_user.php
+++ assets/php/update_user.php
@@ -30,12 +30,12 @@ mysqli_close($conn);
     <title>Update Scraper</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="finalcss.css">	
+    <link rel="stylesheet" type="text/css" href="../css/style.css">	
 
 </head>
 <body>
 <?php include 'session_verification.php'?>
-<?php include 'navigation.php'?>
+<?php include 'partials/navigation.php'?>
 <br>
 <div class="container-fluid">
 <div id="update-container" style="width:50%">
@@ -72,9 +72,9 @@ mysqli_close($conn);
     </form>
 </div>
 </div>
-<?php include 'footer.php'?>
+<?php include 'partials/footer.php'?>
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="darkmode.js"></script>
+<script src="../js/darkmode.js"></script>
 </html>
